dfc385d5f4b59deaba0b18212e5a01e3f2948cad
Delffie <delffie2005@gmail.com>
2025-08-12 18:19:52 +0530

    Time: 16 ms (18.88%), Space: 49.4 MB (86.79%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int majoritytime=nums.length/2;
+        
+        for(int i=0;i<nums.length;i++){
+           map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            if(map.get(nums[i])>majoritytime){
+            return nums[i];
+            }
+        
+        }
+        return -1;
+        
+
+        
+    }
+}
